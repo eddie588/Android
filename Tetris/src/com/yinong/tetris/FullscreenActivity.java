224e4af336f6a1a8f4d13bcf6dacc75ea7f6abe5
eddie588 <eddie_588@hotmail.com>
2013-05-27 20:23:07 -0400

    Don't show title

--- Tetris/src/com/yinong/tetris/FullscreenActivity.java
+++ Tetris/src/com/yinong/tetris/FullscreenActivity.java
@@ -3,6 +3,8 @@ package com.yinong.tetris;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.yinong.tetris.model.TetrisGameListener;
 
@@ -20,9 +22,14 @@ public class FullscreenActivity extends Activity implements TetrisGameListener {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
+        // requesting to turn the title OFF
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        // making it full screen
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_fullscreen);
-		//getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+
+		
 		//final View controlsView = findViewById(R.id.fullscreen_content_controls);
 		
 		gameBoard = (GameBoard) findViewById(R.id.fullscreen_content);
