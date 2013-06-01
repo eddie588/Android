a9f44f9610c69c990af73d876922beb04de4bed8
eddie588 <eddie_588@hotmail.com>
2013-06-01 06:50:03 -0400

    New class for color

---
+++ CubeGame/src/com/yinong/cubegame/util/GLColor.java
@@ -0,0 +1,17 @@
+package com.yinong.cubegame.util;
+
+public class GLColor {
+	
+	public float red;
+	public float green;
+	public float blue;
+	public float alpha;
+
+	public GLColor(float red,float green,float blue,float alpha) {
+		this.red = red;
+		this.green = green;
+		this.blue = blue;
+		this.alpha = alpha;
+	}
+
+}
